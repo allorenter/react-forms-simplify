1c36eb210ccc427a194f47bfeecb2ad3d679be45
Adolfo <allorenter@gmail.com>
2022-11-26 20:36:47 +0100

    Instalación de pendencias principales

---
+++ src/examples/index.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import './index.css';
+
+function Example() {
+  return (
+    <div className='example'>
+      <h1>EXAMPLE</h1>
+      <div></div>
+    </div>
+  );
+}
+
+export default Example;
---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom/client'
+import Example from './examples/index';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <Example />
+  </React.StrictMode>
+)
+

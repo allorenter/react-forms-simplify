b531e8a1878fc340d5c4c384295b53ffe29e099f
Adolfo <allorenter@gmail.com>
2022-12-10 16:00:41 +0100

    Función para transformar FormValues a FormFields

---
+++ src/logic/transformFormValuesToFormFields.test.ts
@@ -0,0 +1,32 @@
+import { describe, test, expect } from 'vitest';
+import transformFormValuesToFormFields from './transformFormValuesToFormFields';
+
+describe('transformFormValuesToFormFields', () => {
+  test('should returns an empty object for an empty input object', () => {
+    expect(transformFormValuesToFormFields({})).toEqual({});
+  });
+
+  test('should returns a single key-value pair for a single-level input object', () => {
+    expect(transformFormValuesToFormFields({ key: 'value' })).toEqual({ key: 'value' });
+  });
+
+  test('should returns multiple key-value pairs with dot-separated keys for a multi-level input object', () => {
+    const input = {
+      key1: 'value1',
+      key2: {
+        nestedKey1: 'nestedValue1',
+        nestedKey2: {
+          nestedNestedKey1: 'nestedNestedValue1',
+        },
+      },
+    };
+
+    const expected = {
+      key1: 'value1',
+      'key2.nestedKey1': 'nestedValue1',
+      'key2.nestedKey2.nestedNestedKey1': 'nestedNestedValue1',
+    };
+
+    expect(transformFormValuesToFormFields(input)).toEqual(expected);
+  });
+});
---
+++ src/logic/transformFormValuesToFormFields.ts
@@ -0,0 +1,18 @@
+function transformFormValuesToFormFields(formValues: Record<string, any>) {
+  const paths: Record<string, any> = {};
+
+  for (const [key, value] of Object.entries(formValues)) {
+    if (typeof value === 'object') {
+      const nestedPaths = transformFormValuesToFormFields(value);
+      for (const [nestedKey, nestedValue] of Object.entries(nestedPaths)) {
+        paths[`${key}.${nestedKey}`] = nestedValue;
+      }
+    } else {
+      paths[key] = value;
+    }
+  }
+
+  return paths;
+}
+
+export default transformFormValuesToFormFields;

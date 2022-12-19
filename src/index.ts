6e27b5974709c3a48189665f472560ffb4e3284f
Adolfo <allorenter@gmail.com>
2022-12-19 21:53:55 +0100

    Export de la libería

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import useBindFormField from './hooks/useBindFormField';
+import useForm from './hooks/useForm';
+import useFormContext from './hooks/useFormContext';
+import useFormErrors from './hooks/useFormErrors';
+import useTouchedFormFields from './hooks/useTouchedFormFields';
+import useWatchFormField from './hooks/useWatchFormField';
+
+export {
+  useForm,
+  useFormContext,
+  useBindFormField,
+  useFormErrors,
+  useTouchedFormFields,
+  useWatchFormField,
+};
+
+export * from './types/Form';

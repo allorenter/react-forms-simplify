4489ffc0e26a187ba3388be12d2eda4baac37366
Adolfo <allorenter@gmail.com>
2023-01-24 22:12:54 +0100

    Merge

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom/client';
-import Example from './examples/index';
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <Example />
-  </React.StrictMode>,
-);

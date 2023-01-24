4a3bd3158cadcc5050bb168825fdbafe85c5c540
Adolfo <allorenter@gmail.com>
2023-01-24 19:24:51 +0100

    Configuro rama main

--- src/main.tsx
+++
@@ -1,6 +0,0 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom/client';
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode></React.StrictMode>,
-);

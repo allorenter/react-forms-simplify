84dd59e4b12cd0aa9ee8c7e4168dbea299ef8d11
Adolfo <allorenter@gmail.com>
2023-06-06 22:30:20 +0200

    Elimino tipos innecesarios

--- src/types/Utils.ts
+++ src/types/Utils.ts
@@ -8,57 +8,7 @@ export type NestedValue<
   [$NestedValue]: never;
 } & TValue;
 
-export type Primitive = null | undefined | string | number | boolean | symbol | bigint;
-
-export type EmptyObject = {
-  [K in string | number]: never;
-};
-export type NonUndefined<T> = T extends undefined ? never : T;
-
-export type LiteralUnion<T extends U, U extends Primitive> =
-  | T
-  | (U & {
-      _?: never;
-    });
-
-export type RecursivePartial<T> = T extends Array<infer U>
-  ? Array<RecursivePartial<U>>
-  : T extends ReadonlyArray<infer U>
-  ? ReadonlyArray<RecursivePartial<U>>
-  : T extends {
-      [key in keyof T]: T[key];
-    }
-  ? {
-      [K in keyof T]?: RecursivePartial<T[K]>;
-    }
-  : T;
-
-export type IsAny<T> = boolean extends (T extends never ? true : false) ? true : false;
-
-export type RecursiveMap<T, TValue> = {
-  [K in keyof T]?: IsAny<T[K]> extends true
-    ? any
-    : NonUndefined<T[K]> extends NestedValue | Date | FileList
-    ? TValue
-    : NonUndefined<T[K]> extends object
-    ? RecursiveMap<T[K], TValue>
-    : NonUndefined<T[K]> extends Array<infer U>
-    ? IsAny<U> extends true
-      ? Array<any>
-      : U extends NestedValue | Date | FileList
-      ? Array<TValue>
-      : U extends object
-      ? Array<RecursiveMap<U, TValue>>
-      : Array<TValue>
-    : TValue;
-};
-
-export type IsFlatObject<T extends object> = Extract<
-  Exclude<T[keyof T], NestedValue | Date | FileList>,
-  any[] | object
-> extends never
-  ? true
-  : false;
+export type Primitive = null | string | number | boolean | symbol | bigint;
 
 export type Digits = '0' | NonZeroDigits;
 

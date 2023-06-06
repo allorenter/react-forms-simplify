4244a44255cf2f99505b8837ee6d0aac5932946b
Adolfo <allorenter@gmail.com>
2023-06-06 20:18:52 +0200

    Tipos para useValue

--- src/hooks/useValue.ts
+++ src/hooks/useValue.ts
@@ -1,13 +1,11 @@
 import { useEffect, useState } from 'react';
 import { Values, FormName, UseForm } from '@/types/Form';
+import { FieldPath, FieldPathValue } from '@/types/Utils';
 
-function useValue<TFormValues extends Values = Values>({
-  name,
-  form,
-}: {
-  name: FormName<TFormValues>;
-  form: UseForm<TFormValues>;
-}) {
+function useValue<
+  TFormValues extends Values = Values,
+  TName extends FieldPath<TFormValues> = FieldPath<TFormValues>,
+>({ name, form }: { name: TName; form: UseForm<TFormValues> }): FieldPathValue<TFormValues, TName> {
   const [value, setValue] = useState<any>();
   const { valuesSubscriptions } = form.$instance;
 
